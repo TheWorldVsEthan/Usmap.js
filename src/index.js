c819b7a7bab951809c1d72bc90bea6a26a58fc5c
Ethanツ <46152296+TheWorldVsEthan@users.noreply.github.com>
2021-03-28 17:27:00 -0400

    Starting on BrotliDecompression

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const mappings = require('./lib/UsmapDownloader');
-const mappingsGrabber = new mappings();
+const UsmapDownloader = require('./lib/UsmapDownloader');
+const mappingsGrabber = new UsmapDownloader();
+
+const BrotliDecompression = require('./lib/BrotliDecompression');
+const decompressor = new BrotliDecompression();
 
 module.exports = class UsmapJS {
     constructor() { };
 
     async start() {
         await mappingsGrabber.grabUsmap();
+        await decompressor.decompressUsmap();
     };
 };
--- src/lib/BrotliDecompression.js
+++ src/lib/BrotliDecompression.js
@@ -1,1 +1,13 @@
-// Haven't Started Yet, Feel Free To Contribute tho :)
+const brotli = require('brotli');
+const fs = require('fs');
+
+module.exports = class BrotliDecompression {
+    constructor() { };
+
+    async decompressUsmap() {
+        // Usmap magic is 0x30C4
+        
+        const output = fs.readFileSync('./mappings/mappings.usmap');
+        console.log(output.toJSON());
+    }
+};
